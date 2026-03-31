dd0089606b84e15ce0615ffee2572eee1002d94f
josephmj0303 <joseph.mj@aol.com>
2026-03-31 13:20:46 +0000

    fix nodeapi keys.js

--- app/node-api/config/keys.js
+++ app/node-api/config/keys.js
@@ -1,6 +1,4 @@
 module.exports = {
- // mongoURI: "<INSERT DATABASE URI>",
-  //  mongoURI: "mongodb://emongo:27017/epoc",
-  mongoURI: "mongodb://emongo:27017/epoc",
+  mongoURI: process.env.MONGO_URL || "mongodb://mongo:27017/epoc",
   secretOrKey: "k1234e6s78h9av"
 };
